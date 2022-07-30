fcbcbb1a744b0a93d3c8e6cc899e4693b6b58f95
Justina Mandaro <bautistajustina7@gmail.com>
2022-07-30 14:16:05 -0700

    added code to pass tests

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(){
+    return 2000/100;
+}
+divide();
+
+const square = x => x * x;
+console.log(square(5));
+
+const add = (num1, num2) => num1 + num2;
+console.log(add(2,3));
